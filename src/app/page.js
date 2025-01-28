45329de8452fe69e3d8b431f8c6d13fd04f631fa
NEIL EVERETTE <neileverette@NEILs-MacBook-Air.local>
2025-01-27 19:41:04 -0600

    Initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
+'use client';
+import { Button } from '@carbon/react';
 
 export default function Home() {
   return (
     <div>
-Hello Carbon! Well, not quite yet. This is the starting point for the Carbon NextJS tutorial.
+      <h1>
+        Hello Neil. This is your Carbon demo! Well, not quite yet. This is the
+        starting point for the Carbon NextJS tutorial.
+      </h1>
+      <Button>Button</Button>
     </div>
-
-  )
+  );
 }
